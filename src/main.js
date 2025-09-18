91c98a5071b939c127cb0b1d89b8eb23ce52b18d
Robert Vásquez <vasquezrober09@gmail.com>
2025-09-18 16:08:31 -0400

    Avance en la selección de ataque

--- src/main.js
+++ src/main.js
@@ -2,9 +2,39 @@
 // Solo cuando cargue el Html se carga el script
 window.addEventListener('load', () => {
 
+	// Almacena el tipo de ataque
+	let ataqueJugador;
+
+	// Función para generar un numero aleatorio del 1 al 6
+	const aleatorio = (min, max) => {
+		return Math.floor(Math.random() * (max - min + (1)) + min);
+	};
+
+	// Selecciona de manera aletoria una Criatura Enemiga
+	const seleccionarMascotaEnemigo = () => {
+		let ataqueAleatorio = aleatorio(1,6);
+
+		let mascotaEnemigo = document.getElementById("mascota-enemigo");
+
+		if(ataqueAleatorio == 1) {
+			mascotaEnemigo.innerHTML = "'Hipodoge', 'Elemento: Agua 💦',";
+		}else if(ataqueAleatorio == 2) {
+			mascotaEnemigo.innerHTML = "'Capipepo', 'Elemento: Tierra 🌱',";
+		}else if(ataqueAleatorio == 3) {
+			mascotaEnemigo.innerHTML = "'Ratigueya', 'Elemento: Fuego 🔥',";
+		}else if(ataqueAleatorio == 4) {
+			mascotaEnemigo.innerHTML = "'Langostelvis', 'Elemento:  Agua 💦 y Fuego 🔥',";
+		}else if(ataqueAleatorio == 5) {
+			mascotaEnemigo.innerHTML = "'Tucapalma', 'Elemento: Agua 💦 y Tierra 🌱',";
+		}else if(ataqueAleatorio == 6){
+			mascotaEnemigo.innerHTML = "'Pydos', 'Elemento: Tierra 🌱 y Fuego 🔥',";
+		}
+	};
+
+	// Obtenemos el boton selecionar
 	let botonMascotaJugador = document.getElementById("boton-mascota");
 
-	//  Seleccionamos la Criatura
+	//  Seleccionamos la Criatura para el jugador
 	botonMascotaJugador.addEventListener('click', () => {
 
 		let inputHipodoge = document.getElementById("Hipodoge");
@@ -16,27 +46,49 @@ window.addEventListener('load', () => {
 
 		let mascotaJugador = document.getElementById("mascota-jugador");
 
+		// Validación de criatura selecionada
 		if(inputHipodoge.checked){
-			alert("Selecionaste: Hipodoge - Agua 💦");
 			mascotaJugador.innerHTML = "'Hipodoge', 'Elemento: Agua 💦',";
 		}else if(inputCapipepo.checked){
-			alert("Selecionaste: Capipepo - Tierra 🌱");
 			mascotaJugador.innerHTML = "'Capipepo', 'Elemento: Tierra 🌱',";
 		}else if(inputRatigueya.checked){
-			alert("Selecionaste: Ratigueya - Fuego 🔥");
 			mascotaJugador.innerHTML = "'Ratigueya', 'Elemento: Fuego 🔥',";
 		}else if(inputLangostelvis.checked){
-			alert("Selecionaste: Langostelvis -  Agua 💦 y Fuego 🔥");
 			mascotaJugador.innerHTML = "'Langostelvis', 'Elemento:  Agua 💦 y Fuego 🔥',";
 		}else if(inputTucapalma.checked){
-			alert("Selecionaste: Tucapalma - Agua 💦 y Tierra 🌱");
 			mascotaJugador.innerHTML = "'Tucapalma', 'Elemento: Agua 💦 y Tierra 🌱',";
 		}else if(inputPydos.checked){
-			alert("Selecionaste: Pydos - Tierra 🌱 y Fuego 🔥");
 			mascotaJugador.innerHTML = "'Pydos', 'Elemento: Tierra 🌱 y Fuego 🔥',";
 		}else{
 			alert("No Seleccionaste una Criatura")
 		}
+
+		// Despues que el jugador selecciona una criatura, se selecciona de forma aleatoria una enemiga
+		seleccionarMascotaEnemigo();
+
 	});
+
+
+	// Creamos la ĺogica para los botones de ataque
+	let botonFuego = document.getElementById("boton-fuego");
+	botonFuego.addEventListener("click", () => {
+		ataqueJugador = "Fuego"
+		alert(ataqueJugador);
+	});
+
+	let botonAgua = document.getElementById("boton-agua");
+	botonAgua.addEventListener("click", () => {
+		ataqueJugador = "Agua"
+		alert(ataqueJugador);
+	});
+
+	let botonTierra = document.getElementById("boton-tierra");
+	botonTierra.addEventListener("click", () => {
+		ataqueJugador = "Tierra"
+		alert(ataqueJugador);
+	});
+
+	
+
 
 });
